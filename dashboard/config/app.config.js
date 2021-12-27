5dbefbeac65aabd103b51d6969f0933cb5538a47
蒋颖 <jiangying@saicmobility.com>
2021-12-27 17:45:28 +0800

    修改说明

--- dashboard/config/app.config.js
+++ dashboard/config/app.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     buildsPerPage: 12,
     dashboardContent:
-        "Visual Testing服务的在线报告仪表板，将所有历史测试构建可视化， " +
-        "make it much easier to Rebase & Debase according to your frequently changed requirements",
+        "Visual Testing服务的在线报告仪表板，将所有历史测试构建可视化， "
 };
